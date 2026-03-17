be2856e96431902da869c48e0bf90b19e34f2a80
lyucean <lyucean@gmail.com>
2026-03-17 11:33:44 +0300

    Initial Commit

---
+++ YaTray/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YaTray
+//
+//  Created by Admin on 17.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YaTray/YaTrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaTrayApp.swift
+//  YaTray
+//
+//  Created by Admin on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct YaTrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
